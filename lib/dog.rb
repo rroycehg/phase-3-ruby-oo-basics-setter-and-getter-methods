645123ce43744d6b0a979af42cf35929b2e913d5
Roli <rroycehg@gmail.com>
2021-07-19 17:52:31 -0400

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
+    def name=(dog_name)
+      @name = dog_name
+    end
+
+    def name
+      @name
+    end
+    
+    fido = Dog.new
+    fido.name = "Fido"
+
+    fido.name
+
+    def breed=(breed_name)
+        @breed = breed_name
+    end
+
+    def breed
+        @breed
+    end
+
+    snoopy = Dog.new
+    snoopy.breed = "Beagle"
+
+    snoopy.breed
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
+    def name=(persons_name)
+        @name = persons_name
+    end
+
+    def name
+        @name
+    end
+
+    beyonce = Person.new
+    beyonce.name = "Beyonce"
+
+    def job=(jobs_name)
+        @job = jobs_name
+    end
+
+    def job
+        @job
+    end
+
+    beyonce.job = "Singer"
 
 end
